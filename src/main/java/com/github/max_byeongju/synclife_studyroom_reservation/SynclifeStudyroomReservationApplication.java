aad1e1cac7b39ed5f08984bb3a1a59e0b8c6111f
max-byeongju <max-byeongju@users.noreply.github.com>
2025-09-26 13:49:22 +0900

    Initial commit

---
+++ src/main/java/com/github/max_byeongju/synclife_studyroom_reservation/SynclifeStudyroomReservationApplication.java
@@ -0,0 +1,13 @@
+package com.github.max_byeongju.synclife_studyroom_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynclifeStudyroomReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynclifeStudyroomReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/max_byeongju/synclife_studyroom_reservation/SynclifeStudyroomReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.max_byeongju.synclife_studyroom_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynclifeStudyroomReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
